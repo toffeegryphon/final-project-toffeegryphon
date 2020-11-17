07265334401ea2a6457fe2878f430a636101579f
Nairen <nairenfu@gmail.com>
2020-11-17 17:22:00 +0800

    Implemented main game scaffolding and basic controls.

--- include/epidemic_app.h
+++ include/epidemic_app.h
@@ -1,32 +1,43 @@
 #pragma once
 
-#include <cinder/gl/gl.h>
 #include <cinder/app/App.h>
+#include <cinder/gl/gl.h>
 #include <city.h>
-#include <unordered_set>
+#include <hand.h>
+#include <isolation.h>
 
 namespace epidemic {
 
 using ci::app::App;
-using glm::vec2;
-using std::unordered_set;
 using cinder::app::MouseEvent;
+using glm::vec2;
+using std::pair;
 
 class EpidemicApp : public App {
  public:
   EpidemicApp();
 
+  void setup() override;
   void update() override;
   void draw() override;
   void mouseDown(MouseEvent event) override;
   void mouseUp(MouseEvent event) override;
+  void mouseMove(MouseEvent event) override;
+  void mouseDrag(MouseEvent event) override;
 
  private:
   const vec2 kWindowSize{800, 800};
-  const vec2 kCitySize{500, 500};
-  const vec2 kCityOffset{20, 20};
+  const Location::Data kCityData{vec2{500, 500}, vec2{20, 20}};
   City city_;
-  unordered_set<Individual*> grabbed_;
+
+  // TODO Vector tie to each Isolation
+  const Location::Data kTemplateIsolationData{vec2{200, 100}, vec2{20, 20}};
+  // TODO Maybe isolation manager
+  vector<pair<Isolation, Location::Data>> isolations_;
+  // TODO Maybe here contain unordered_set of individuals,
+  // or have a separate handler class. Everything else just have
+  // vector of appropriate pointers.
+  Hand hand_;
 };
 
 }  // namespace epidemic
--- src/epidemic_app.cc
+++ src/epidemic_app.cc
@@ -1,32 +1,99 @@
+#include <configuration.h>
 #include <epidemic_app.h>
+#include <utils.h>
 
 namespace epidemic {
 
 using ci::gl::clear;
 using cinder::app::MouseEvent;
+using utils::IsInLocation;
 
-EpidemicApp::EpidemicApp() : city_(kCitySize) {
+EpidemicApp::EpidemicApp() : city_(kCityData.size) {
   srand(static_cast<unsigned int>(time(nullptr)));
 
   ci::app::setWindowSize(static_cast<int>(kWindowSize.x),
                          static_cast<int>(kWindowSize.y));
+
+  // TODO Own private method
+  vec2 offset(
+      kCityData.offset.x + kCityData.size.x + kTemplateIsolationData.offset.x,
+      kTemplateIsolationData.offset.y);
+  for (size_t i = 0; i < Configuration::kIsolationCount; ++i) {
+    isolations_.emplace_back(
+        Isolation(kTemplateIsolationData.size),
+        Location::Data{kTemplateIsolationData.size, offset});
+    offset.y += kTemplateIsolationData.size.y + kTemplateIsolationData.offset.y;
+  }
+}
+
+void EpidemicApp::setup() {
+  AppBase::setup();
 }
 
 void EpidemicApp::update() {
   city_.Update();
+
+  for (pair<Isolation, Location::Data>& isolation : isolations_) {
+    isolation.first.Update();
+  }
 }
 
 void EpidemicApp::draw() {
   clear();
-  city_.Draw(kCityOffset);
+  for (const pair<Isolation, Location::Data>& isolation : isolations_) {
+    isolation.first.Draw(isolation.second.offset);
+  }
+
+  city_.Draw(kCityData.offset);
+  hand_.Draw();
 }
 
 void EpidemicApp::mouseDown(MouseEvent event) {
   vec2 pos = event.getPos();
-  // TODO binary search
+  if (IsInLocation(pos, kCityData)) {
+    hand_.Add(city_.ExtractIndividualsAt(pos - kCityData.offset));
+    hand_.Update(pos);
+    return;
+  }
+
+  for (pair<Isolation, Location::Data>& isolation : isolations_) {
+    if (IsInLocation(pos, isolation.second)) {
+      hand_.Add(
+          isolation.first.ExtractIndividualsAt(pos - isolation.second.offset));
+      hand_.Update(pos);
+      return;
+    }
+  }
 }
+
 void EpidemicApp::mouseUp(MouseEvent event) {
-  AppBase::mouseUp(event);
+  vec2 pos = event.getPos();
+  if (IsInLocation(pos, kCityData)) {
+    hand_.Update(pos - kCityData.offset);
+    city_.Add(hand_.Release());
+    return;
+  }
+
+  for (pair<Isolation, Location::Data>& isolation : isolations_) {
+    if (IsInLocation(pos, isolation.second)) {
+      hand_.Update(pos - isolation.second.offset);
+      // Handle overflow TODO maybe better way to write
+      hand_.Add(isolation.first.Add(hand_.Release()));
+      break;
+    }
+  }
+
+  pos -= kCityData.offset;
+  hand_.Update(pos);
+  city_.Add(hand_.Release());
+}
+
+void EpidemicApp::mouseMove(MouseEvent event) {
+  hand_.Update(event.getPos());
+}
+
+void EpidemicApp::mouseDrag(MouseEvent event) {
+  hand_.Update(event.getPos());
 }
 
 }  // namespace epidemic
